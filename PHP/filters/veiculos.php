112e7c6108d0c8a9c3c0ecbbf75e205bf24cd5cb
kenjiorita1 <kenjiorita3@gmail.com>
2025-06-16 23:10:08 -0300

    fix: versão par

--- PHP/filters/veiculos.php
+++ PHP/filters/veiculos.php
@@ -33,7 +33,7 @@ if (!$resultVeiculos) {
         <link rel="stylesheet" href="../assets/css/style.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet"
             integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+        <link rel="stylesheet" href="./PHP/assets/css/all.css">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap-theme.min.css" integrity="sha384-6pzBo3FDv/PJ8r2KRkGHifhEocL+1X2rVCTTkUfGk7/0pbek5mMa1upzvWbrUbOZ" crossorigin="anonymous">
     </head>
 
--- index.php
+++ index.php
@@ -50,9 +50,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>AutoElite - Sistema Administrativo</title>
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="./PHP/assets/css/bootstrap.css" rel="stylesheet">
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="stylesheet" href="./PHP/assets/css/all.css">
     <link rel="stylesheet" href="./PHP/assets/css/style.css">
 </head>
 <body>
@@ -177,7 +177,7 @@
         </div>
     </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="./PHP/assets/js/bootstrap.bundle.min.js"></script>
 
     <script>
         document.querySelector('.toggle-sidebar').addEventListener('click', function() {
